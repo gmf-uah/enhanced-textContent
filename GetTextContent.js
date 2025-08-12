6cbb1b92c568ccbe25e28dbfbf46de148ddb34b7
Grant <gmf0010@uah.edu>
2025-08-12 13:50:13 -0500

    removed some comments

--- GetTextContent.js
+++ GetTextContent.js
@@ -1,9 +1,8 @@
 function GetTextContent(node, include = false, filter = () => include) {
     let text = "";
     // If node matches filter and include is true, or doesn't match and include is false
-    // if ((include && matchesFilter(node)) || (!include && !matchesFilter(node))) {
+    // if ((include && filter(node)) || (!include && !filter(node))) {
     if (include == filter(node)) { // this is more concise but less readable
-        // console.log("test");
         for (const child of node.childNodes) {
             if (child.nodeType === Node.TEXT_NODE) {
                 node_text = child.nodeValue.trim();
@@ -20,24 +19,6 @@ function GetTextContent(node, include = false, filter = () => include) {
     }
     return text;
 }
-
-// console.log(document.querySelector(".poop").textContent)
-// console.log("bruh")
-// console.log(document.querySelector("#A").childNodes)
-// console.log(document.querySelector("h1"))
-// for (const child of document.querySelector("#A").childNodes) {
-//     if (child.nodeType === Node.TEXT_NODE) {
-//         console.log(child.nodeValue);
-//     }
-// }
-
-// TODO: loop through childNodes and get text nodes 
-
-// console.log(GetTextContent(
-//     document.querySelector("#A"),
-//     false,
-//     (el) => (el.classList.contains("poop"))
-// ));
 
 console.log(GetTextContent(
     document.querySelector("#A"),
